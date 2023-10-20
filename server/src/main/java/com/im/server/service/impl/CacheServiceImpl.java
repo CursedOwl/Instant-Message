cc14d846ceb857bd8394a855dc137636a63ce48a
Cursed_Owl <cymogeko@163.com>
2023-10-21 00:48:38 +0800

    Update

--- server/src/main/java/com/im/server/service/impl/CacheServiceImpl.java
+++ server/src/main/java/com/im/server/service/impl/CacheServiceImpl.java
@@ -1,15 +1,16 @@
 package com.im.server.service.impl;
 
 import com.im.server.service.CacheService;
+import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.util.function.Function;
 
+@Service
 public class CacheServiceImpl implements CacheService {
     @Override
     public <R, ID> R queryWithoutThrough(String KEY_PREFIX, ID id, Class<R> clazz, Duration ttl, Function<ID, R> function) {
 
-
-
+        return null;
     }
 }
--- server/src/main/java/com/im/server/service/impl/GroupServiceImpl.java
+++ server/src/main/java/com/im/server/service/impl/GroupServiceImpl.java
@@ -1,9 +1,11 @@
 package com.im.server.service.impl;
 
 import com.im.server.service.GroupService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class GroupServiceImpl implements GroupService {
     @Override
     public List<Integer> getMembers(Integer group) {
